c0db885181cc4e02382e4401c46b31b2a9420851
Anna <cc241022@fhstp.ac.at>
2026-04-08 17:26:18 +0200

    Update input.js

--- script/input.js
+++ script/input.js
@@ -4,14 +4,14 @@ const input = {};
 
 input.click = function(event){
     //plays audio que
-    let crunch = new Audio("../images/cronch.mp4");
+    let crunch = new Audio("Assignment_Minesweeper/images/cronch.mp4");
             crunch.play();
     global.blocks10[this.parentNode.rowIndex][this.cellIndex].clickReact();
     
 }
 input.rClick = function(event){
     //plays audio que
-    let mark = new Audio("../images/hitwood.mp4");
+    let mark = new Audio("Assignment_Minesweeper/images/hitwood.mp4");
     mark.play();
     global.blocks10[this.parentNode.rowIndex][this.cellIndex].rClickReact();
     //console.log(global.blocks10[this.parentNode.rowIndex][this.cellIndex]);
